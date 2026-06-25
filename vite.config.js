61025b29e4087b001c98d41714f143a9ad2f5e8e
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2026-06-25 11:37:54 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  root: './',
+  publicDir: 'public',
+
+  plugins: [],
+  build: {
+    outDir: 'dist', 
+    assetsDir: 'assets',
+    sourcemap: false, 
+    target: 'es2024', 
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        pure_funcs: ['console.log'], 
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg|webp|avif)$/.test(name ?? '')) {
+            return 'assets/images/[name]-[hash][extname]';
+          }
+          if (/\.css$/.test(name ?? '')) {
+            return 'assets/css/[name]-[hash][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+  },
+
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
