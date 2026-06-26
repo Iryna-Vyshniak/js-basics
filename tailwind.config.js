8a88fd1af7697399d37b01f142a142a143cd7a19
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2026-06-26 22:30:39 +0300

    Update styles for result box and code block; enhance dialog dropdown animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,17 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx,html}" 
   ],
   theme: {
     extend: {
       colors: {
         primary: {
+          50: '#eff6ff', 
+          200: '#bfdbfe', 
           DEFAULT: '#2563eb', 
-          hover: '#1d4ed8',
+          hover: '#1d4ed8', 
+          800: '#1e40af'  
         }
       }
     },
